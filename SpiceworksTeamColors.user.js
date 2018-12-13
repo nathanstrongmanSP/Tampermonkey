8005d734e249b18c3ca57b1623ae8c963deca7e2
Nathan Strongman <det0nat3@users.noreply.github.com>
2018-12-13 13:08:39 -0400

    Update SpiceworksTeamColors.user.js

--- SpiceworksTeamColors.user.js
+++ SpiceworksTeamColors.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Spiceworks Team Colors
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.1
 // @description  Adds Team Colors
 // @author       Nathan Strongman nathanstrongman@outlook.com
 // @match        help.starport.ca/*
