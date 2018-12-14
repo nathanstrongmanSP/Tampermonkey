9b8ccba49a55cceec9243e3030467c85c9556d43
Nathan Strongman <det0nat3@users.noreply.github.com>
2018-12-14 12:18:47 -0400

    Create

---
+++ SpiceworksDashboard.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Spiceworks Dashboard
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Starport Spiceworks dashboard
+// @author       Nathan Strongman nathanstrongman@outlook.com
+// @match        help.starport.ca/*
+// @match        help4.starport.ca/*
+// @updateURL    https://raw.githubusercontent.com/det0nat3/tampermonkey/master/SpiceworksDashboard.user.js
+// @downloadURL  https://raw.githubusercontent.com/det0nat3/tampermonkey/master/SpiceworksDashboard.user.js
+// @run-at       document-end
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle ( `
+    /* "New Ticket" & "Refresh Ticket" buttons */
+    .sui-bttn-toolbar{
+        display: none;
+    }
+    /* Search Box */
+    .page-search {
+        display: none;
+    }
+    /* Ticket Table  */
+    .ticket-table-wrapper {
+        height: 600px;
+    }
+    /* Bottom ticket Pane */
+    .sui-opt-in .helpdesk-app .ticket-pane {
+        display: none;
+    }
+    /* Table scroll bars */
+    .sui-opt-in .helpdesk-app .ticket-grid .table-body .ticket-table-wrapper {
+        overflow: hidden;
+    }
+` );
