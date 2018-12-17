28ea468e9375589e4ed0d2aa4f753246abd59535
Nathan Strongman <det0nat3@users.noreply.github.com>
2018-12-17 14:46:58 -0400

    Added URL

--- SpiceworksCleanup.user.js
+++ SpiceworksCleanup.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Spiceworks Cleanup
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.2.1
 // @description  Removes Ads and other junk
 // @author       Nathan Strongman nathanstrongman@outlook.com
 // @match        help.starport.ca/*
+// @match        http://star-dc1-help1/*
 // @match        help4.starport.ca/*
 // @updateURL    https://raw.githubusercontent.com/det0nat3/tampermonkey/master/SpiceworksCleanup.user.js
 // @downloadURL  https://raw.githubusercontent.com/det0nat3/tampermonkey/master/SpiceworksCleanup.user.js
