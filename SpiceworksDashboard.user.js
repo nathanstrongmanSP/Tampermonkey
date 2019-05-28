05d31acaa07dd931bf5a621956137c3fdc535e49
Nathan Strongman <nathan.strongman@tech.starport.local>
2019-05-28 09:05:18 -0300

    Added active hours

--- SpiceworksDashboard.user.js
+++ SpiceworksDashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Spiceworks 3-Dashboard
 // @namespace    http://tampermonkey.net/
-// @version      2.7.0
+// @version      2.7.1
 // @description  Starport Spiceworks dashboard
 // @author       Nathan Strongman (nathan.strongman@starport.ca)
 // @match        help.starport.ca/tickets/*
@@ -88,19 +88,28 @@ var h1 = header[0].getElementsByTagName("h1");
 var h1HTML = h1[0].innerHTML.replace("Tickets <","<");
 h1[0].innerHTML = h1HTML;
 
-// Only make noise after 9 AM Mon-Fri
-var today = new Date();
-var hour = today.getHours();
-var day = today.getDay();
-
-if (hour > 8 && day != 0 && day != 9)
+// Only make noise 9-9 Mon-Fri
+function activeHours()
 {
-    setInterval(function(){
+    var today = new Date();
+    var hour = today.getHours();
+    var day = today.getDay();
+    if (hour > 8 && hour < 21 && day != 0 && day != 9)
+    {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+setInterval(function(){
+    if (activeHours())
+    {
         console.log('Making some noise if tickets exist...')
         var ticket = document.getElementsByClassName("column-id");
         if (ticket.length > 1) {
             var audio = new Audio('https://notificationsounds.com/message-tones/to-the-point-568/download/mp3');
         audio.play();
         }
-    }, 60000);
-}
+    }
+}, 60000);
