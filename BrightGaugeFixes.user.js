ea13df171092b609473aa738109ae1214e381e15
Nathan Strongman <49568801+nathanstrongmanSP@users.noreply.github.com>
2023-03-17 09:22:20 -0300

    Update BrightGaugeFixes.user.js

--- BrightGaugeFixes.user.js
+++ BrightGaugeFixes.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         BrightGauge Fixes
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Add missing features that should be there...
 // @author       Nathan Strongman nathan.strongman@starport.ca
 // @match        https://sp.brightgauge.co/public/dashboards/*
+// @match        https://*.brightgauge.co/dashboards/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=brightgauge.co
 // @updateURL    https://github.com/nathanstrongmanSP/Tampermonkey/raw/master/BrightGaugeFixes.user.js
 // @downloadURL  https://github.com/nathanstrongmanSP/Tampermonkey/raw/master/BrightGaugeFixes.user.js
